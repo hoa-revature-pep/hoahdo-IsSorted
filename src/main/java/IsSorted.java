48704af254c243b659e5f9797b43e38aa5d3a35e
Hoa Do <hoa.do.ma@gmail.com>
2023-08-07 17:23:09 +0000

    Completed Challenge: Is Sorted

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,15 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        
+        for (int idx = 0; idx < arr.length-1; idx++) {
+            if(arr[idx] < arr[idx+1]) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
